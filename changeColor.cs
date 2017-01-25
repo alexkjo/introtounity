5e75fd528557615c7b0b8a2ff09fdc32ba5a8102
Unknown <10681564@my.uvu.edu>
2017-01-25 11:06:12 -0700

    Added New Unity project

---
+++ changeColor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeColor : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+            print("Change Color to red.");
+        }
+        if (Input.GetKeyDown(KeyCode.G)){
+            GetComponent<Renderer>().material.color = Color.green;
+            print("Change color to green.");
+        }
+            if (Input.GetKeyDown(KeyCode.B)) {
+                GetComponent<Renderer>().material.color = Color.blue;
+                print("Change color to blue.");
+            }
+            if (Input.GetKeyDown(KeyCode.Delete)) {
+                GetComponent<Renderer>().material.color = Color.white;
+                print("Change color to White.");
+            }
+            if (Input.GetKeyDown(KeyCode.Space)){
+                GetComponent<Renderer>().material.color = Color.black;
+                print("Change color to black.");
+            }
+            if (Input.GetKeyDown(KeyCode.KeypadEnter)){
+                GetComponent<Renderer>().material.color = Color.yellow;
+                print("Change color to Yellow.");
+            }
+    }
+}
