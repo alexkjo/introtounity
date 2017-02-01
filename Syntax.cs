65125efc44d8154169de5f272402f554f84ac4e0
Unknown <10681564@my.uvu.edu>
2017-02-01 10:57:57 -0700

    Added Syntax 1&2

---
+++ Syntax.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Syntax : MonoBehaviour
+{
+
+    // Class identifiers are sentence casing
+    // For more than one word UseCamelCasing
+    void Shootlaser()
+    {
+        print("Hello World!");
+
+        if (2 + 2 == 4)
+        {
+            print("It's true");
+        }
+        //Double equals means to compare
+        //Single equal means to assign value
+
+    }
+
+    void ShootBlaster()
+    {
+        print("Ouch!");
+    }
+    void ShootBow()
+    {
+        print("Thats gotta hurt!");
+
+        if (5 + 10 == 15)
+        {
+            print("That's correct!");
+        }
+    }
+
+    void ShootRailGun()
+    {
+        print("He's probably dead.");
+    }
+}
---
+++ Syntax2.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Syntax2 : MonoBehaviour {
+
+    //Strings always start with a lower case.
+    //Modifier, identifier, function, variable
+    public string myString = "10";
+    public string myOtherString = "1";
+    public int myInt = 1;
+    public float myFloat = 1.2f;
+    public bool myBool = false;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
