e2191c5bf821385ad61898c47e02e698ecfbff2f
Unknown <10681564@my.uvu.edu>
2017-02-10 10:53:27 -0700

    Added Switches

---
+++ Switches.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switches : MonoBehaviour {
+
+
+    public enum ColorsChoices {Red, Green, Blue };
+    public ColorsChoices PlayerColor;
+
+    public enum GameStates
+    {
+        LOADING,
+        STARTING,
+        PLAYING,
+        ENDING
+    }
+
+    public GameStates currentGameState = GameStates.LOADING;
+
+	void Start () {
+
+        switch (PlayerColor)
+        {
+            case ColorsChoices.Red:
+                print("This gives access to all power-ups");
+                break;
+
+            case ColorsChoices.Green:
+                print("This gives access to all weapons");
+                break;
+
+            case ColorsChoices.Blue:
+                print("Just Dies");
+                break;
+
+            default:
+                break;
+        }
+	}
+	
+
+}
