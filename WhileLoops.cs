000b959c65b0b40574ff479841b1602a84baee50
Unknown <10681564@my.uvu.edu>
2017-02-15 10:52:23 -0700

    Added While Loops

---
+++ WhileLoops.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhileLoops : MonoBehaviour {
+
+    int i = 0;
+    public string[] colors;
+    
+    // While loops are for accessing a lot of code.
+	void Start () {
+        while (i < 10)
+        {
+            print(colors [i]);
+            i++;
+        }
+
+        print("Done.");
+
+    // This will go through the whole array instead of just till 9.
+        while (i < colors.Length)
+        {
+            print(colors[i]);
+            i++;
+        }
+
+        print("Done.");
+
+	}
+	
+}
