168748a26080e48606554961f59a64e6846c9592
Unknown <10681564@my.uvu.edu>
2017-02-17 10:45:37 -0700

    Added Button and mouse input script

---
+++ ButtonAndMouseInput.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonAndMouseInput : MonoBehaviour {
+
+    // Void is the , mymouseclick is an identicator
+    public void MyMouseClick ()
+    {
+        print("I clicked the button.");
+    }
+
+}
