13b97414de028cb4a2fe072a549cb8349c3c1783
Unknown <10681564@my.uvu.edu>
2017-03-03 10:07:46 -0700

    Added all files

---
+++ KeyInput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Input is a class, GetKey is a function, and Keycode is an arguement.
+	void Update () {
+	    if(Input.GetKey(KeyCode.A))
+        {
+            print("I hit A.");
+        }
+	}
+}
---
+++ Lists.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Lists : MonoBehaviour {
+
+    public List<string> myWeaponList;
+    public string[] myWeaponArray;
+
+    void Start(){
+        myWeaponList.Add("Gun");
+        myWeaponList.Remove("Club");
+        myWeaponList.Add("Bow");
+        myWeaponList.RemoveAt(1);
+    }
+
+}
