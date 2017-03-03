0a47916964dba8617948b4dd9172e634ac07289b
Unknown <10681564@my.uvu.edu>
2017-03-03 10:40:01 -0700

    Added Logical Operators

---
+++ LogicalOperators.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogicalOperators : MonoBehaviour {
+
+    public string string1;
+    public string string2;
+
+
+    // Use this for initialization
+    public void OnButtonClick()
+    {
+        if (string1 == "Thing1" && string2 == "Thing2")
+        {
+            print("These things are quite tame.");
+        }
+
+        if (string1 == "Thing1" || string2 == "Thing2")
+        {
+            print("It's fun to have fun if you know what to do.");
+        }
+
+        if (string1 != "Thing1" && string2 != "Thing2")
+        {
+            print("Oh what a shame, what a shame, what a shame.");
+        }
+
+    }
+}
